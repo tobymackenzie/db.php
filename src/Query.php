8828e632a07063d8058530ab30d6e7f2bc9392a0
Toby Mackenzie <public@tobymackenzie.com>
2020-09-12 20:27:25 -0400

    feat: add `Query` object for separately handling query part of statement

---
+++ src/Query.php
@@ -0,0 +1,39 @@
+<?php
+namespace TJM\DB;
+use Exception;
+class Query{
+	protected $params;
+	protected $sql;
+	public function __construct($query = null){
+		if(is_string($query)){
+			$this->setSql($query);
+		}else{
+			$this->set($query);
+		}
+	}
+	public function __toString(){
+		return $this->getSql();
+	}
+	public function getParameters(){
+		return $this->params ?: Array();
+	}
+	public function hasParameters(){
+		return (bool) $this->params;
+	}
+	public function setParameters($value = null){
+		$this->params = $value;
+	}
+	protected function buildSql(){
+		return null;
+	}
+	public function getSql(){
+		if(is_string($this->sql)){
+			return $this->sql;
+		}else{
+			return $this->buildSql();
+		}
+	}
+	public function setSql($value){
+		$this->sql = $value;
+	}
+}
--- src/Statement.php
+++ src/Statement.php
@@ -6,12 +6,11 @@ use InvalidArgumentException;
 use PDO;
 use PDOStatement;
 class Statement{
-	protected $params;
 	protected $query;
 	protected $result;
 	protected $statement;
 	public function __construct($opts = null){
-		if(is_string($opts)){
+		if(is_string($opts) || $opts instanceof Query){
 			$this->setQuery($opts);
 		}elseif($opts){
 			$this->set($opts);
@@ -42,6 +41,8 @@ class Statement{
 		}elseif($set instanceof PDOStatement){
 			$this->setStatement($set);
 			$this->setQuery($set->queryString);
+		}elseif($set instanceof Query){
+			$this->setQuery($set);
 		}elseif($set instanceof Statement){
 			$this->setQuery($set->getQuery());
 			$this->setResult($set->getResult());
@@ -55,18 +56,24 @@ class Statement{
 		}
 	}
 	public function getParameters(){
-		return $this->params ?: Array();
+		return $this->hasQuery() ? $this->getQuery()->getParameters() : Array();
 	}
 	public function hasParameters(){
-		return (bool) $this->params;
+		return $this->hasQuery() ? $this->getQuery()->hasParameters() : false;
 	}
 	public function setParameters($value = null){
-		$this->params = $value;
+		$this->hasQuery() ? $this->getQuery()->getParameters() : Array();
 	}
 	public function getQuery(){
 		return $this->query;
 	}
+	public function hasQuery(){
+		return (bool) $this->query;
+	}
 	public function setQuery($value = null){
+		if(isset($value) && !($value instanceof Query)){
+			$value = new Query($value);
+		}
 		$this->query = $value;
 	}
 	public function getResult(){
@@ -76,8 +83,8 @@ class Statement{
 		$this->result = $result;
 	}
 	public function getSql(){
-		if($this->query){
-			return $this->query;
+		if($this->hasQuery()){
+			return $this->getQuery()->getSql();
 		}elseif($this->statement){
 			return $this->statement->queryString;
 		}else{
